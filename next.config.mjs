3b810ec39ddcff3ddc3a33d030bf299cbf62bd11
jags <jagadeesh.geerla@gmail.com>
2026-02-18 01:58:33 +0530

    Fix: Add basePath /shredder to fix subdirectory deployment styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/shredder',
+    assetPrefix: '/shredder',
+    trailingSlash: true,
 };
 
 export default nextConfig;
